e7d3cebace872ccb6129d27995cdd7e6f9d0abcf
bachkhoabk47 <bachkhoabk47@gmail.com>
2016-06-20 14:42:59 +0700

    Update code all-in-one-container

---
+++ Dockerize-OpenCPS/all-in-one-container/docker-compose/app-data/server/tomcat-7.0.62/temp/5-opencps-portlet/WEB-INF/service/org/opencps/dossiermgt/service/dossierServiceUtil.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.opencps.dossiermgt.service;
+
+import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+import com.liferay.portal.service.InvokableService;
+
+/**
+ * Provides the remote service utility for dossier. This utility wraps
+ * {@link org.opencps.dossiermgt.service.impl.dossierServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author trungnt
+ * @see dossierService
+ * @see org.opencps.dossiermgt.service.base.dossierServiceBaseImpl
+ * @see org.opencps.dossiermgt.service.impl.dossierServiceImpl
+ * @generated
+ */
+public class dossierServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link org.opencps.dossiermgt.service.impl.dossierServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static java.lang.Object invokeMethod(java.lang.String name,
+		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
+		throws java.lang.Throwable {
+		return getService().invokeMethod(name, parameterTypes, arguments);
+	}
+
+	public static void clearService() {
+		_service = null;
+	}
+
+	public static dossierService getService() {
+		if (_service == null) {
+			InvokableService invokableService = (InvokableService)PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(),
+					dossierService.class.getName());
+
+			if (invokableService instanceof dossierService) {
+				_service = (dossierService)invokableService;
+			}
+			else {
+				_service = new dossierServiceClp(invokableService);
+			}
+
+			ReferenceRegistry.registerReference(dossierServiceUtil.class,
+				"_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	public void setService(dossierService service) {
+	}
+
+	private static dossierService _service;
+}
